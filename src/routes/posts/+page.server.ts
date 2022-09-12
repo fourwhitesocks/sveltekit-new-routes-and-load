4c6776947e6c60aadaab5965bb40823660c9a822
amy stout <asbilly92@gmail.com>
2022-09-12 10:26:24 -0600

    removed my public hygraph link

--- src/routes/posts/+page.server.ts
+++ src/routes/posts/+page.server.ts
@@ -2,7 +2,7 @@ import { postquery } from '$lib/data/db';
 //postquery is the function from db.js
 
 export async function load() {
-  //const response = await fetch('https://api-us-east-1.graphcms.com/v2/cl3dd3gfx27qu01xnfag6c432/master');
+  //const response = await fetch('https:had-graph-cms-master-link');
 
   //const users = await response.json();
   //console.log(users)
